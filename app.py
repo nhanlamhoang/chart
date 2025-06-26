3ada85c39c57a47e22b6bc118fa96a97d5c0acc7
nhanlamhoang <126466940+nhanlamhoang@users.noreply.github.com>
2025-06-26 11:57:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,15 @@
 import dash
 from dash import html
 
-# tạo đối tượng app là callable
+# Tạo đối tượng Dash
 app = dash.Dash(__name__)
 
+# Gắn layout
 app.layout = html.Div([
     html.H1("Ứng dụng Dash đầu tiên của bạn")
 ])
 
-# KHÔNG gọi app.run() hoặc app.run_server()
-# vì Render sẽ tự làm qua gunicorn
+# Gắn server để Gunicorn có thể nhận ra
+server = app.server  # <-- DÒNG NÀY QUAN TRỌNG!
+
+# KHÔNG gọi app.run()
